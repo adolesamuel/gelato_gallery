dd990a1a32a36552daa1f17eb16033a499bf1f18
adolesamuel <adolesamuel4@gmail.com>
2021-11-14 15:41:31 +0100

    Create strings.dart

---
+++ lib/core/utils/strings.dart
@@ -0,0 +1,12 @@
+//holds strings used in the app
+class AppStrings {}
+
+///Holds strings used for error messages
+class ErrorStrings {
+  static const notAJson = 'Not a JSON';
+  static const jsonSerializationError = 'Error Serializing JSON';
+  static const noInternetError = 'No Internet';
+  static const timeOutError = 'The request timed out';
+  static const unhandledException = 'An Error Occured, We are working on it';
+  static const serverError = 'Server Error';
+}
