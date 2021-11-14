73dc6dd140193efdabdaebfec03e4617bbe6e6ed
adolesamuel <adolesamuel4@gmail.com>
2021-11-14 15:39:34 +0100

    Update api_endpoints.dart

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -1,3 +1,7 @@
+///A class for declaring endpoints  used in this task
 class Endpoint {
-  static const imageList = '';
+  ///This Endpoint returns a list of paginated and counted list of photos
+  ///consider including params for page and limit(number of photos)
+  ///for example: https://picsum.photos/v2/list?page=1&limit=100
+  static const photosList = 'https://picsum.photos/v2/list';
 }
